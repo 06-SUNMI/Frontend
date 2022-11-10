63bc7d9670074fd3dcbd47743a908a0bf0727277
김승준 <june7662@ajou.ac.kr>
2022-11-10 23:38:30 +0900

    change structure

---
+++ lib/ui/screen/challenge_infomation.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:sunmi/main.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:sunmi/ui/screen/challenge_date.dart';
+
+////// Tempbutton = 챌린지 목록 확인 임의로 만들어 둔 페이지, 나중에 지울게요
+class Tempbutton extends StatelessWidget{
+  // late Sdays selectdays;
+
+  String _selectcays = "3";
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('challenge'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () async{
+                _selectcays = "3";
+                Get.to(() => Challenge_check(), arguments: _selectcays);
+              },
+              child: Text("주3회 챌린지"),
+              style: TextButton.styleFrom(
+                primary: Colors.blueGrey,
+              ),
+            ),
+            TextButton(
+              onPressed: () async{
+                _selectcays= "5";
+                Get.to(() => Challenge_check(), arguments: _selectcays);
+              },
+              child: Text("  주5회 챌린지"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+class Challenge_check extends StatelessWidget {
+  var _selectcays = Get.arguments;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("everyhealth")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Center(
+                  child:Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children : <Widget>[
+                      //makeImage(BoxFit.scaleDown),
+                      Image.asset('assets/images/challenge.png',fit: BoxFit.scaleDown),
+                    ],
+                  )
+              ),
+              // height: 300,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    width: 200, // 가로 설정
+                    height: 400, // 높이 설정
+                    color: Colors.white,
+                    child: Text("11월 28일 ~ 12월 26일 \n"+"주 $_selectcays 일, 한 달 동안 ", style: TextStyle(fontSize: 10,fontFamily: 'Signatra',
+                        fontWeight: FontWeight.normal),textAlign: TextAlign.center,),
+                    padding: EdgeInsets.all(12),
+                  ),
+                  SizedBox(
+                    child : Container(
+                      color: Colors.white,
+                      child: ElevatedButton(
+                        //   child:
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            primary: Colors.red,
+                            onPrimary: Colors.white,
+                            minimumSize: Size(150,100),
+
+                            alignment: Alignment.center,
+                            textStyle: const TextStyle(fontSize: 20)
+                        ),
+                        child:Text("신청하기"),
+                        onPressed: (){
+                          Get.to(() => ChallengeDatePage()/*, arguments: _selectcays*/);
+                          Colors.green;
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    //Center(child: Text("$selectdays"),),
+    //  body: hallendge_checkpage(selectdays),
+
+    // );
+
+  }
+
+}
--- lib/ui/screen/challenge_list_page.dart
+++ lib/ui/screen/challenge_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ChallengeListPage extends StatelessWidget {
   ChallengeListPage({Key? key}) : super(key: key);
@@ -6,6 +7,144 @@ class ChallengeListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('챌린지 리스트 페이지'),
+      ),
+      body: GetX<ChallengeController>(
+        init: Get.put(ChallengeController(challengeRepository: ChallengeRepository(challengeProvider: ChallengeProvider()))),
+        builder: (challengeController) {
+          print(challengeController.challenges.length);
+          return GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: DefaultTabController(
+              length: 2,
+              initialIndex: 0,
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.grey,
+                    child: TabBar(
+                        labelColor: Colors.black,
+                        indicator: const BoxDecoration(color: Colors.white),
+                        tabs: [challengeTab(), registeredChallengeTab()]),
+                  ),
+                  Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: TabBarView(
+                          children: [challengeListView(), registeredChallengeListView()],
+                        ),
+                      )
+                  ),
+                ]
+              )
+            ),
+          );},
+      ),
+    );
+  }
+}
+
+challengeTab(){
+  return Tab(text: '신청가능한 챌린지');
+}
+
+registeredChallengeTab(){
+  return Tab(text: '신청한 챌린지',);
+}
+
+challengeListView() {
+  return Expanded(
+      child: ListView.separated(
+        //shrinkWrap: true,
+        itemCount: Get.find<ChallengeController>().challenges.length ,
+        itemBuilder: (context, index) {
+          return InkWell(
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/checkimage.png',
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
+                Column(
+                  children: [
+                    Text(
+                        '${Get.find<ChallengeController>().challenges[index].challengeName}'),
+                    Text(
+                        '${Get.find<ChallengeController>().challenges[index].startDate}'
+                            '\nto ${Get.find<ChallengeController>().challenges[index].endDate}'),
+                  ],
+                )
+              ],
+            ),
+            onTap: ()=>Get.find<ChallengeController>().challengeDetails(index),
+          );},
+        separatorBuilder: (context, index) {
+          return const Divider(
+            color: Colors.black,
+            thickness: 1.0,
+          );
+        }
+      )
+  );
+}
+
+registeredChallengeListView(){
+  return Expanded(
+      child: ListView.separated(
+        //shrinkWrap: true,
+        itemCount: Get.find<ChallengeController>().registeredChallenges.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: ()=>Get.find<ChallengeController>().registeredChallengeDetails(index),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Image.asset(
+                  'assets/images/checkimage.png',
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
+                Column(
+                    children: [
+                      Text(
+                          '${Get.find<ChallengeController>().registeredChallenges[index].challengeName}'),
+                      Text(
+                          '${Get.find<ChallengeController>().registeredChallenges[index].startDate}'
+                              '\nto ${Get.find<ChallengeController>().registeredChallenges[index].endDate}'),
+                    ]
+                )
+              ],
+            ),
+          );},
+        separatorBuilder: (context, index){
+          return const Divider(
+            color: Colors.black,
+            thickness: 1.0,
+          );
+        }
+      )
+  );
+}
+
+class RegisteredChallengeDetailsPage extends StatelessWidget{
+  RegisteredChallengeDetailsPage({Key? key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+          child: InkWell(
+            onTap: () => Get.back(),
+            child: Text('신청한 챌린지 ${Get.arguments['index']}의 상세정보 '),
+          )
+      ),
+    );
+>>>>>>> Stashed changes
   }
 }
