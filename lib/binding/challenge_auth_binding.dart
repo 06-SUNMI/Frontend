21fd2f0503ebe6390058604cbc95cdf93c2fbdd0
김승준 <june7662@ajou.ac.kr>
2022-11-21 15:22:36 +0900

    fixed image overflow

--- lib/binding/challenge_auth_binding.dart
+++ lib/binding/challenge_auth_binding.dart
@@ -1,8 +1,13 @@
 import 'package:get/get.dart';
+import 'package:sunmi/controller/challenge_auth_controller.dart';
+import 'package:sunmi/data/provider/challenge_auth_provider.dart';
+import 'package:sunmi/data/repository/challenge_auth_repository.dart';
 
 class ChallengeAuthBinding extends Bindings {
   @override
   void dependencies() {
-    // TODO: implement dependencies
+    Get.lazyPut(() => ChallengeAuthController(
+        challengeAuthRepository: ChallengeAuthRepository(
+            challengeAuthProvider: ChallengeAuthProvider())));
   }
 }
